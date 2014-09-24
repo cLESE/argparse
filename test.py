b812fbe706f3d298982e2d29f653545a3cfe4d5f
Clément Sébillet <clement.sebillet@bts-malraux.net>
2014-09-24 15:34:44 +0200

    up verification

--- test.py
+++ test.py
@@ -7,37 +7,29 @@ parser.add_argument("nomfichier", help="nom donner a la playlist")
 parser.add_argument("formatfichier", help="extension de la playlist", choices=['m3u', 'xspf', 'pls'])
 
 """argument optionnel"""
-parser.add_argument("-G", "--genre", help="genre voulu dans la playlist")
-parser.add_argument("-P", "--pctgenre", help="pourcentage du genre voulu dans la playlist", type=float)
-parser.add_argument("-g", "--sousgenre", help="sous genre voulu dans la playlist")
-parser.add_argument("-p", "--pctsousgenre", help="pourcentage du sous genre voulu dans la playlist", type=float)
-parser.add_argument("-A", "--artiste", help="artiste voulu dans la playlist")
-parser.add_argument("-Z", "--pctartiste", help="pourcentage de l'artiste voulu dans la playlist", type=float)
+parser.add_argument("-G", "--genre", help="genre et pourcentage du genre voulu dans la playlist", nargs=2)
+parser.add_argument("-g", "--sousgenre", help="sous genre et pourcentage du sous genre voulu dans la playlist", nargs=2)
+parser.add_argument("-A", "--artiste", help="artiste et pourcentage de l'artiste voulu dans la playlist", nargs=2)
 parser.add_argument("-a", "--album", help="album voulu dans la playlist")
 parser.add_argument("-t", "--titre", help="titre voulu dans la playlist")
 
 args = parser.parse_args()
+
+def verifPourcentage(arg):
+    return int(arg)
 
 print (args.temps)
 print (args.nomfichier)
 print (args.formatfichier)
 
 if args.genre:
-    print (args.genre)
-    if args.pctgenre:
-        print (args.pctgenre)
+    verifPourcentage(args.genre[1])
+    print(args.genre)
 if args.sousgenre:
-    print (args.sousgenre)
-    if args.pctsousgenre:
-        print (args.pctsousgenre)
+    print(args.sousgenre)
 if args.artiste:
-    print (args.artiste)
-    if args.pctartiste:
-        print (args.pctartiste)
+    print(args.artiste)
 if args.album:
-    print (args.album)
+    print(args.album)
 if args.titre:
-    print (args.titre)
-
-
-    """test"""
+    print(args.titre)
